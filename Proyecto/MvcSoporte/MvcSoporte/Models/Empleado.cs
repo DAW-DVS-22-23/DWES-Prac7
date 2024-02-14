acf4f00903f59bf1833ce4850ef4c5779ec671d1
DAW-DVS-22-23 <valdesd1314@gmail.com>
2024-02-14 18:41:37 +0100

    Añadimos Empleado y TipoAveria

--- Proyecto/MvcSoporte/MvcSoporte/Models/Empleado.cs
+++ Proyecto/MvcSoporte/MvcSoporte/Models/Empleado.cs
@@ -1,6 +1,22 @@
-﻿namespace MvcSoporte.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcSoporte.Models
 {
     public class Empleado
     {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre del empleado es un campo requerido.")]
+        public string? Nombre { get; set; }
+        [Display(Name = "Correo Electrónico")]
+        [EmailAddress(ErrorMessage = "Dirección de correo electrónico iválida.")]
+        public string? Email { get; set; }
+        [Display(Name = "Teléfono")]
+        public string? Telefono { get; set; }
+        [Display(Name = "Fecha de nacimiento")]
+        [DataType(DataType.Date)]
+        public DateTime? FechaNacimiento { get; set; }
+
+        public ICollection<Aviso>? Avisos { get; set; }
+
     }
 }
