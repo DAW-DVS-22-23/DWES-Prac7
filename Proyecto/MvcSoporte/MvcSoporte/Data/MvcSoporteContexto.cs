0f9f770cc9ab9eb952ba68bdcabf05149cd6d3c0
DAW-DVS-22-23 <valdesd1314@gmail.com>
2024-02-14 20:15:42 +0100

    Hemos creado el contexto y el modelo

---
+++ Proyecto/MvcSoporte/MvcSoporte/Data/MvcSoporteContexto.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcSoporte.Models;
+
+namespace MvcSoporte.Data
+{
+    public class MvcSoporteContexto : DbContext
+    {
+        public MvcSoporteContexto(DbContextOptions<MvcSoporteContexto> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Aviso>? Avisos { get; set; }
+        public DbSet<Empleado>? Empleados { get; set; }
+        public DbSet<Equipo>? Equipos { get; set; }
+        public DbSet<Localizacion>? Localizaciones { get; set; }
+        public DbSet <TipoAveria>? TipoAverias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aviso>().ToTable("Aviso");
+            modelBuilder.Entity<Empleado>().ToTable("Empleado");
+            modelBuilder.Entity<Equipo>().ToTable("Equipo");
+            modelBuilder.Entity<Localizacion>().ToTable("Localizacion");
+            modelBuilder.Entity<TipoAveria>().ToTable("TipoAveria");
+
+
+            base.OnModelCreating(modelBuilder);
+            foreach (var relationship in
+            modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+
+
+    }
+}
